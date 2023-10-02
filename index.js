4114055e9d387c1cf6600b10e31a500f0bafa18e
Hayden Roney <roneyhayden@gmail.com>
2023-10-02 10:41:02 -0500

    lab finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+const array = [`John`, `Bob`, `Smith`, `Sarah`];
+
+function findMatching(arrayOfNames, findValue) {
+    return arrayOfNames.filter((name) => name.toLowerCase() === findValue.toLowerCase())
+}
+
+function fuzzyMatch(arrayOfNames, findValue) {
+    return arrayOfNames.filter((name) => name.startsWith(findValue, 0))
+}
+
+function matchName(arrayOfObjects, findValue) {
+    return arrayOfObjects.filter((value) => value.name === findValue)
+}
